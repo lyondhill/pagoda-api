a4e3e5c3c4fa0b14b85621e33be8b821366ec6eb
Lyon <lyon@delorum.com>
2011-09-16 14:28:16 -0600

    commit to work on monitor for a minute

--- lib/pagoda-client/apis/app.rb
+++ lib/pagoda-client/apis/app.rb
@@ -6,8 +6,54 @@ module Pagoda
         true
       end
 
+
+#############
+#  Get
+#############
+
       def app_list
         json get("/apps")
+      end
+
+      def app_info(app)
+        
+      end
+
+#############
+#  Post
+#############
+
+      def app_create(name, git_url)
+        
+      end
+
+
+#############
+#  Put
+#############
+      def app_deploy_latest(app)
+        
+      end
+
+      def app_deploy(app, branch, commit)
+        
+      end
+
+      def app_rewind(app)
+        
+      end
+
+      def app_fast_forward(app)
+        
+      end
+
+
+#############
+#  Delete
+#############
+
+      def app_destroy(app)
+        
       end
 
     end
