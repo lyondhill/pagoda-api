9564a66d5fdc995f44d6acf3bdb37da0690dbc37
Lyon <lyon@delorum.com>
2011-09-19 16:45:01 -0600

    push new api functions

--- lib/pagoda-client/apis/app.rb
+++ lib/pagoda-client/apis/app.rb
@@ -34,6 +34,15 @@ module Pagoda
       def app_create(name, git_url)
         json post("/apps", {:app => {:name => name, :git_url => git_url}})
       end
+
+      def scale_up(app, qty=1)
+        put("/apps/#{app}/scale-up", {:quantity => qty})
+      end
+      
+      def scale_down(app, qty=1)
+        put("/apps/#{app}/scale-down", {:quantity => qty})
+      end
+
       
 #############
 #  Put
